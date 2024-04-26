b96774b44366ca7f38578129a29e0317ecb83b97
G4bizinha <gabivonborowski@gmail.com>
2024-04-26 00:46:20 -0300

    commit 26.04.2024

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,22 +1,50 @@
 <template>
-    <div> 
-        <h1>Posts</h1>
-        <div v-for="post in posts" :key="post.id" class="">
-            <h2 class="text-2xl font-bold">{{ post.title }}</h2>
-            <p class="font-mono">{{ post.body }}</p>
+    <div>
+      <h1>Posts</h1>
+      <div v-for="post in posts" :key="post.id" class="p-2">
+        <h2 class="text-2xl font-bold">{{ post.title }}</h2>
+        <p class="font-mono">{{ post.body }}</p>
+        <div class="flex py-2">
+          <LikeOff class="w-8 h-8" />
+          <Comments class="w-8 h-8" />
         </div>
+  
+        <div class="px-6 flex">
+          <div class="bg-zinc-100 p-1 rounded-full"></div>
+          <div>
+            <template v-if="comments.some(comment => comment.postId === post.id)">
+              <div v-for="comment in comments" :key="comment.id" class="p-2">
+                <div v-if="comment.postId === post.id">
+                  <p class="font-bold">{{ comment.name }}</p>
+                  <p class="font-mono">{{ comment.body }}</p>
+                </div>
+              </div>
+            </template>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+  </template>
+  
+  <script>
+  import LikeOff from "@/assets/LikeOff.vue";
+  import Comments from "@/assets/Comments.vue";
+  import { defineComponent } from "vue";
+  export default defineComponent({
     props: {
-        posts: {
-            type: Array,
-            required: true
-        }
-    }
-});
-</script>
+      posts: {
+        type: Array,
+        required: true,
+      },
+      comments: {
+        type: Array,
+        required: true,
+      },
+    },
+    components: {
+      LikeOff,
+      Comments,
+    },
+  });
+  </script>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="p-12">
     <h1 class="font-black text-2xl">Minha rede.com</h1>
-    <Posts :posts="postsData"/>
+    <Posts :posts="postsData" :comments="commentsData"/>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import Posts from '@/components/Posts.vue'; 
-import { getAllPosts } from '@/services/Api';
+import { getAllPosts, getAllComments } from '@/services/Api';
 
 export default defineComponent({
     components: {
@@ -16,16 +16,25 @@ export default defineComponent({
     },
     data() {
         return {
-            postsData: []
+            postsData: [],
+            commentsData: []
         };
     },
     mounted() {
       this.getPosts();
+      this.getAllComments();
     },
     methods: {
         async getPosts() {
             try {
                 this.postsData = await getAllPosts();
+            } catch (error) {
+                console.log(error);
+            } 
+        },
+        async getAllComments() {
+            try {
+                this.commentsData = await getAllComments();
             } catch (error) {
                 console.log(error);
             } 
