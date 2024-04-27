bea59fbc3b247af0e6beaef7a7916102dc25230c
G4bizinha <gabivonborowski@gmail.com>
2024-04-27 00:24:11 -0300

    ViteCommit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
 <script>
 import { defineComponent } from 'vue';
 import Posts from '@/components/Posts.vue'; 
-import { getAllPosts, getAllComments } from '@/services/Api';
+import { getAllPosts, getAllComments, getAllAlbums, getAllPhotos, getAllUsers } from '@/services/Api';
 
 export default defineComponent({
     components: {
@@ -17,12 +17,19 @@ export default defineComponent({
     data() {
         return {
             postsData: [],
-            commentsData: []
+            commentsData: [],
+            albumsData: [],
+            photosData: [],
+            usersData: []
         };
     },
     mounted() {
       this.getPosts();
       this.getAllComments();
+      this.getAllAlbums();
+      this.getAllPhotos();
+      this.getAllUsers();
+
     },
     methods: {
         async getPosts() {
@@ -38,7 +45,31 @@ export default defineComponent({
             } catch (error) {
                 console.log(error);
             } 
-        }
+        },
+
+        async getAllAlbums() {
+            try {
+                this.albumsData = await getAllAlbums();
+            } catch (error) {
+                console.log(error);
+            } 
+        },
+
+        async getAllPhotos() {
+            try {
+                this.photosData = await getAllPhotos();
+            } catch (error) {
+                console.log(error);
+            } 
+        },
+
+        async getAllUsers() {
+            try {
+                this.usersData = await getAllUsers();
+            } catch (error) {
+                console.log(error);
+            } 
+        },
     }
 });
 </script>
